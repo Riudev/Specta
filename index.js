7c9c22704c8f10fe3c48783e5d7b1066201d2593
Rei <102203066+Riudev@users.noreply.github.com>
2022-05-04 23:36:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,103 +3,81 @@ const { Client, Collection, MessageEmbed,MessageAttachment } = require(`discord.
 const { readdirSync } = require(`fs`);
 const { join } = require(`path`);
 const db = require('quick.db');
-const { keep_alive } = require("./keep_alive");
-const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
+const { keep_alive } = require("./server");
+const { PREFIX, COLOR, MAINCOLOR } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login(TOKEN);
+
 client.commands = new Collection();
 client.prefix = PREFIX;
 client.queue = new Map();
 const cooldowns = new Collection();
 const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
 
-//this fires when the BOT STARTS DO NOT TOUCH
-client.on(`ready`, () => {	
+client.on(`ready`, () => {
+    setInterval(() => {
+    let member;
+    client.guilds.cache.forEach(async guild => {
+      await delay(15);
+      member = await client.guilds.cache.get(guild.id).members.cache.get(client.user.id)
 
-   
-   ///////////////////////////////
-    ////////////IFCHEMPTY//////////
-        //remove everything in between those 2 big comments if you want to disable that the bot leaves when ch. or queue gets empty!
-        setInterval(() => { 
-          let member;
-        client.guilds.cache.forEach(async guild =>{
-        await delay(15);
-          member = await client.guilds.cache.get(guild.id).members.cache.get(client.user.id)
-        //if not connected
-          if(!member.voice.channel)
-          return;
-        //if alone 
-        if (member.voice.channel.members.size === 1) 
-        { return member.voice.channel.leave(); }
-      });
-      
+      if (!member.voice.channel)
+        return;
 
-      client.user.setActivity(`${PREFIX}help | ${client.guilds.cache.size} Server`, { type: "STREAMING",
-      url: "https://www.twitch.tv/nocopyrightsounds"});
-   
-  
-      }, (5000));
-      ////////////////////////////////
-      ////////////////////////////////
-    figlet.text(`${client.user.username} ready!`, function (err, data) {
-      if (err) {
-          console.log('Something went wrong');
-          console.dir(err);
-      }
-      console.log(`═════════════════════════════════════════════════════════════════════════════`);
-      console.log(data)
-      console.log(`═════════════════════════════════════════════════════════════════════════════`);
-    })
-   
+      if (member.voice.channel.members.size === 1) { return member.voice.channel.leave(); }
+    });
+  }, (5000));
+
+  figlet.text(`${client.user.username} ready!`, function(err, data) {
+    if (err) {
+      console.log('Something went wrong');
+      console.dir(err);
+    }
+    console.log(`[info]: ready on client (${client.user.tag})`.rainbow)
+    console.log(`[info]: See`.red, `${client.guilds.cache.size} server`.bgCyan, `${client.channels.cache.size} channel`.bgGreen, `${client.users.cache.size} user`.bgMagenta)
+  })
+
+
 });
-//DO NOT TOUCH
+
 client.on(`warn`, (info) => console.log(info));
-//DO NOT TOUCH
 client.on(`error`, console.error);
-//DO NOT TOUCH
-//FOLDERS:
-//Admin custommsg data FUN General Music NSFW others
-commandFiles = readdirSync(join(__dirname, `Music`)).filter((file) => file.endsWith(`.js`));
-for (const file of commandFiles) {
-  const command = require(join(__dirname, `Music`, `${file}`));
-  client.commands.set(command.name, command);
-}
-commandFiles = readdirSync(join(__dirname, `others`)).filter((file) => file.endsWith(`.js`));
-for (const file of commandFiles) {
-  const command = require(join(__dirname, `others`, `${file}`));
-  client.commands.set(command.name, command);
-}
-//COMMANDS //DO NOT TOUCH
+
+client.commands = new Collection();
+client.aliases = new Collection();
+["commands", "events"].forEach(handler => {
+  try {
+    require(`./handler/${handler}`)(client);
+  } catch (e) {
+    console.log(e)
+  }
+});
+
+client.queue = new Map()
+process.on('unhandledRejection', console.error);
+
 client.on(`message`, async (message) => {
   if (message.author.bot) return;
-  
-  //getting prefix 
+ 
   let prefix = await db.get(`prefix_${message.guild.id}`)
-  //if not prefix set it to standard prefix in the config.json file
   if(prefix === null) prefix = PREFIX;
 
-  //information message when the bot has been tagged
   if(message.content.includes(client.user.id)) {
     message.reply(new Discord.MessageEmbed().setColor("#F0EAD6").setAuthor(`${message.author.username}, My Prefix is ${prefix}, to get started; type ${prefix}help`, message.author.displayAvatarURL({dynamic:true})));
   } 
-  //An embed announcement for everyone but no one knows so fine ^w^
+
   if(message.content.startsWith(`${prefix}embed`)){
-    //define saymsg
     const saymsg = message.content.slice(Number(prefix.length) + 5)
-    //define embed
     const embed = new Discord.MessageEmbed()
-    .setColor("#F0EAD6")
+    .setColor(MAINCOLOR)
     .setDescription(saymsg)
-    .setFooter("Harmony", client.user.displayAvatarURL())
-    //delete the Command
+    .setFooter(`${message.guild.name}`, client.user.displayAvatarURL())
+
     message.delete({timeout: 300})
-    //send the Message
     message.channel.send(embed)
   }
 
 
-//command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
  if (!prefixRegex.test(message.content)) return;
  const [, matchedPrefix] = message.content.match(prefixRegex);
@@ -120,7 +98,7 @@ client.on(`message`, async (message) => {
    if (now < expirationTime) {
      const timeLeft = (expirationTime - now) / 1000;
      return message.reply(
-      new MessageEmbed().setColor("#F0EAD6")
+      new MessageEmbed().setColor(COLOR)
       .setTitle(`:x: Please wait \`${timeLeft.toFixed(1)} seconds\` before reusing the \`${prefix}${command.name}\`!`)    
      );
    }
@@ -144,3 +122,5 @@ function delay(delayInms) {
    }, delayInms);
  });
 }
+
+client.login(prosses.env.Specta);
