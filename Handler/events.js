a606158f120e03986332981149803416ed06056f
Rei <102203066+Riudev@users.noreply.github.com>
2022-05-07 09:12:50 +0700

    Update events.js

--- Handler/events.js
+++ Handler/events.js
@@ -3,9 +3,9 @@ require('colors');
 
 module.exports = (client, Discord) => {
   const load = dirs => {
-    const events = readdirSync(`./events/${dirs}/`).filter(d => d.endsWith("js") );
+    const events = readdirSync(`./Events/${dirs}/`).filter(d => d.endsWith("js") );
     for (let file of events) {
-      let evt = require(`../events/${dirs}/${file}`);
+      let evt = require(`../Events/${dirs}/${file}`);
       let eName = file.split('.')[0];
       client.on(eName, evt.bind(null, client))
       console.log('[Events]'.yellow + ` Loaded ` + eName.green + '✅');
