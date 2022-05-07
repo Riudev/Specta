9b336910639c60e2676fc09d6fc4c17adf86ed9d
Rei <102203066+Riudev@users.noreply.github.com>
2022-05-07 09:15:15 +0700

    Delete ready.js

--- Events.js/ready.js
+++
@@ -1,7 +0,0 @@
-require('colors')
-const { PREFIX } = require(`../../config.json`);
-
-module.exports = async client => {
-	client.user.setActivity(`${PREFIX}help | Specta Cafe`, { type: "STREAMING",
-	       url: "https://discord.gg/kUUQjJRj"});
-};
